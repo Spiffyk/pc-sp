1d4fcbb1fd90c778b8b76d85ebb7ddbc65fdae0f
Spiffyk <oto.stava@gmail.com>
2018-01-06 11:04:54 +0100

    Add default .pgm extension of none is provided

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "pgm.h"
 #include "gmproc.h"
@@ -9,6 +10,9 @@
 #define EXIT_CREATION_ERROR (3)
 #define EXIT_PROCESS_ERROR (4)
 #define EXIT_WRITE_ERROR (5)
+#define EXIT_ALLOC_ERROR (6)
+
+#define PGM_EXT (".pgm")
 
 /**
  * Prints help.
@@ -39,6 +43,59 @@ char* get_filename(char *path) {
 }
 
 /**
+ * Checks whether the given filename (not path!) has an extension.
+ *
+ * @param filename the filename to check
+ * @return 1 if it does have an extension, otherwise 0
+ */
+int filename_has_extension(char *filename) {
+    while(*filename != '\0') {
+        if (*filename == '.') {
+            return 1;
+        }
+
+        filename++;
+    }
+
+    return 0;
+}
+
+/**
+ * Checks whether the filename in the given path has an extension.
+ *
+ * @param path the path to check
+ * @return 1 if it does have an extension, otherwise 0
+ */
+#define has_extension(path) (filename_has_extension(get_filename(path)))
+
+/**
+ * Adds the default extension to the path if there is none yet.
+ *
+ * @param dest the destination pointer (to be assigned)
+ * @param arg the source argument
+ * @param ext the default extension to add
+ * @return 1 if new memory has been allocated, 0 if not, -1 if allocation error has occured
+ */
+int add_extension(char **dest, char *arg, char *ext) {
+    unsigned int buff_len;
+
+    if (has_extension(arg)) {
+        *dest = arg;
+        return 0;
+    }
+
+    buff_len = (unsigned int) strlen(arg) + (unsigned int) strlen(ext) + 1;
+    *dest = calloc(buff_len, sizeof(char));
+    if (!*dest) {
+        return -1;
+    }
+
+    strncpy(*dest, arg, buff_len);
+    strcat(*dest, ext);
+    return 1;
+}
+
+/**
  * Program entry point.
  *
  * @param argc the number of arguments (including the path to the program)
@@ -46,7 +103,7 @@ char* get_filename(char *path) {
  * @return program output
  */
 int main(int argc, char *argv[]) {
-    int s, retval = EXIT_SUCCESS;
+    int s, retval = EXIT_SUCCESS, input_alloc, output_alloc;
     char *input_filename, *output_filename;
     greymap *input_gm, *output_gm;
 
@@ -55,8 +112,13 @@ int main(int argc, char *argv[]) {
         return EXIT_MISSING_ARGS;
     }
 
-    input_filename = argv[1];
-    output_filename = argv[2];
+    input_alloc = add_extension(&input_filename, argv[1], PGM_EXT);
+    output_alloc = add_extension(&output_filename, argv[2], PGM_EXT);
+
+    if (input_alloc == -1 || output_alloc == -1) {
+        printf("Error while allocating memory for filenames!\n");
+        return EXIT_ALLOC_ERROR;
+    }
 
     /* Read input from file */
     printf("-- Reading input greymap from '%s'... ", input_filename);
@@ -105,6 +167,12 @@ int main(int argc, char *argv[]) {
     printf("-- Freeing resources before exit... ");
     greymap_free(&input_gm);
     greymap_free(&output_gm);
+    if (input_alloc) {
+        free(input_filename);
+    }
+    if (output_alloc) {
+        free(output_filename);
+    }
     printf("[ done. ]\n");
 
     return retval;
